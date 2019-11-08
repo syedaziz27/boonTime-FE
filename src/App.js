7da9d2c52c2041ca1b47898aa9d16f809c37cc19
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2019-11-08 06:11:21 -0500

    created secondary navbar according to design specs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
+import themeFile from "./utils/theme";
 import Navbar from "./components/Navbar";
 import TrendingChannels from "./components/trendingChannels/trendingChannels";
 
+//MUI stuff
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme(themeFile);
+
 function App() {
   return (
-    <BrowserRouter>
-      <Route path="/" component={Navbar} />
-      <div className="container">
-        <TrendingChannels />
-      </div>
-    </BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Route path="/" component={Navbar} />
+        <div className="container">
+          <TrendingChannels />
+        </div>
+      </BrowserRouter>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/utils/theme.js
@@ -0,0 +1,16 @@
+export default {
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#CDC8CE",
+      dark: "#008394",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#DFDFDF",
+      dark: "#b22a00",
+      contrastText: "#fff"
+    }
+  }
+};

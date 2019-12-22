9e1508c0b9f5aba66a130e4a938bf07543afea2e
Syed Aziz <syedaziz27x@gmail.com>
2019-12-21 23:16:25 -0500

    fixed file name error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/navbar";
 import TrendingChannels from "./components/trendingChannels/trendingChannels";
 import LandingPage from "./components/LandingPage/landingPage";
 import Products from "./components/Product-recommendations/products";
-import Card from "./components/Card/card";
+import VideoCard from "./components/videoCard/videoCard";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <Route path="/" exact component={LandingPage} />
       <Route path="/vlogger/rec" exact component={Products} />
       {/* NOTE: CHANGE VLOGGER TO USERS ACTUAL USERNAME */}
-      <Route path="/vlogger" exact component={Card} />
+      <Route path="/vlogger" exact component={VideoCard} />
     </BrowserRouter>
   );
 }

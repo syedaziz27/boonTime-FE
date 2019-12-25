72b8551e80a0a3e0f0063878bed05687b4f87ada
Richard Green <richardgreen@pursuit.org>
2019-12-25 14:30:17 -0500

    ..

---
+++ src/components/toggle/toggle.jsx
@@ -0,0 +1,17 @@
+import React from "React";
+
+const toggleVideoRec = () => {
+  return (
+    <div>
+      <div>
+        <h3>Videos</h3>
+      </div>
+
+      <div>
+        <h3>Product Recommendations</h3>
+      </div>
+    </div>
+  );
+};
+
+export default toggleVideoRec;

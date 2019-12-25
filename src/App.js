c63460c488b9ca073cc8d4502a0164bfba5b7da9
Richard Green <richardgreen@pursuit.org>
2019-12-25 14:46:55 -0500

    ..

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import LandingPage from "./components/LandingPage/landingPage";
 import Products from "./components/Product-recommendations/products";
 import VideoCard from "./components/videoCard/videoCard";
 import SignUp from "./components/SignUp/signup";
+import VloggersPage from './pages/VloggersPage/VloggersPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" component={Navbar} />
       <Route path="/" exact component={LandingPage} />
-      <Route path="/vlogger/rec" exact component={Products} />
+      <Route path="/vlogger/rec" exact component={VloggersPage} />
       {/* NOTE: CHANGE VLOGGER TO USERS ACTUAL USERNAME */}
       <Route path="/vlogger" exact component={VideoCard} />
       <Route path="/signup" exact component={SignUp}></Route>

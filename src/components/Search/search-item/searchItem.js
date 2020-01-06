b18145e0ad5e8d0a8709fd84660c024039b38b6d
Jorge Billini <jorgebillini@Pursuits-Air.home>
2020-01-05 23:02:44 -0500

    style changes

--- src/components/Search/search-item/searchItem.js
+++ src/components/Search/search-item/searchItem.js
@@ -7,23 +7,21 @@ export default (props) =>{
                 return(<React.Fragment key={i}>
                             <div className="item-3"></div>
                             <div className="item-2">
-                                <img src={cream} height="50%" width="50%"></img>
+                                <img src={cream}  width="100%"></img>
                             </div>
                             <div className="item-4">
                                     <h1 className="item-12">{e.title}</h1>
                                     <div className="item-12">
-                                    <span>1k Views</span> * <span>3 hours ago</span>
-                                    <span>Skin type: Dry</span>
-                                    <span>Like</span>
-                                    <span>Products used:</span>    
+                                        <span>1k Views</span> * <span>3 hours ago</span>
+                                        <span>Skin type: Dry</span>
+                                        <span>Like</span>
+                                        <span>Products used:</span>
                                     </div>
                                     <div className="row-search">
                                         <img className="item-2" src={cream} width="50%"></img>
                                         <img className="item-2" src={cream} width="50%" width="50%"></img>
                                         <img className="item-2" src={cream} width="50%" width="50%"></img>
-
                                     </div>
-                                
                             </div>
                             <div className="item-3"></div>
                     </React.Fragment>)
--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -8,15 +8,24 @@ export default class Search extends React.Component{
         this.state ={
             results:[],
             query:{text:'',filters:[]},
-
+            loadMore : false,
+            
         }
     }
-    async componentDidMount(){
+
+    async FetchData(page){
         const baseUrl = "https://jsonplaceholder.typicode.com/posts";
         const {query } = this.props.match.params;
         let url = baseUrl+`?userId=${query}`;
         const {data} = await axios.get(url);
-        const queryObj = {text:query,filters:null,};
+
+        return data;
+    }
+    async componentDidMount(){
+        
+        const data = await this.FetchData();
+        let text = this.props.match.params;
+        const queryObj = {text:text,filters:null,};
         this.setState({results:data,query:queryObj},()=>console.log(this.state));
     }
     componentDidUpdate(){

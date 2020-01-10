2c668616403008604fb0d97d81ded8d3344a4b79
jorgebillini <jorgebillini@Pursuits-Air.home>
2020-01-09 23:12:57 -0500

    More styling completed

--- src/components/Search/search-item/searchItem.js
+++ src/components/Search/search-item/searchItem.js
@@ -11,18 +11,29 @@ export default (props) =>{
                             </div>
                             <div className="item-4">
                                     <h1 className="item-12">{e.title}</h1>
-                                    <div className="item-12">
+                                    <div className="row-search">
                                         <span>1k Views</span> * <span>3 hours ago</span>
-                                        <span>Skin type: Dry</span>
-                                        <span>Like</span>
-                                        <span>Products used:</span>
+                                        
                                     </div>
+                                    <div className="row-search used-products">
+                                    <span>Products used:</span>
+                                    </div>
+                                    
+
+                                   
+
+
                                     <div className="row-search">
                                         <img className="item-2" src={cream} width="50%"></img>
                                         <img className="item-2" src={cream} width="50%" width="50%"></img>
                                         <img className="item-2" src={cream} width="50%" width="50%"></img>
+                                        <p className="more-items item-2">Show more</p>
+                                        <div className="row-search item-2">
+                                        <span className="user-metadata-name item-6">Kylie</span>
+                                         <span className=" user-metadata-sub-count item-6">Subscriber count</span>
                                     </div>
-                            </div>
+                                    </div>
+                                                                </div>
                             <div className="item-3"></div>
                     </React.Fragment>)
             })}

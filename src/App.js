7263109145b2834e81c5129a6d5a96356aa18ef8
Syed Aziz <syedaziz27x@gmail.com>
2020-01-18 19:05:13 -0500

    added login component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import LandingPage from "./components/LandingPage/landingPage";
 import Products from "./components/Product-recommendations/products";
 import VideoCard from "./components/videoCard/videoCard";
 import SignUp from "./components/SignUp/signup";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       {/* NOTE: CHANGE VLOGGER TO USERS ACTUAL USERNAME */}
       <Route path="/vlogger" exact component={VideoCard} />
       <Route path="/signup" exact component={SignUp}></Route>
+      <Route path="/login" exact component={Login}></Route>
     </BrowserRouter>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}

5522575c22f7de263f14557b2d3e9877ed181fe0
Richard Green <richardgreen@pursuit.org>
2020-01-23 23:39:28 -0500

    show more button is functional

--- src/components/subscribeUserButton/subscribeUserButton.jsx
+++ src/components/subscribeUserButton/subscribeUserButton.jsx
@@ -3,18 +3,22 @@ import "./subscribeUserButton.css";
 
 export default () => {
   return (
-    <div className="user_container_subscribe">
-      <div className="profile_picture">
-        <img
-          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-          key="user_picture"
-        />
+    <>
+      <div className="user_container_subscribe">
+        <div className="profile_picture">
+          <img
+            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+            key="user_picture"
+            alt="avatar"
+          />
+        </div>
+
+        <div className="name_subscribers">
+          <p className="user_name">AdamsLoveMakeUp</p>
+          <p className="user_subscribers">101 Subscribers</p>
+        </div>
+        <button className="subscribe_button">SUBSCRIBE</button>
       </div>
-      <div className="name_subscribers">
-        <p className="user_name">LoveMakeUp</p>
-        <p className="user_subscribers">101 Subscribers</p>
-      </div>
-      <button className="subscribe_button">SUBSCRIBE</button>
-    </div>
+    </>
   );
 };

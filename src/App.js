408120bb34be0623ad259a8e27a74c5e391c7a0c
Syed Aziz <syedaziz27x@gmail.com>
2020-01-24 20:50:02 -0500

    updated routes

--- src/App.js
+++ src/App.js
@@ -13,21 +13,16 @@ import Search from "./components/Search/search";
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={Navbar} />{" "}
-      <Route path="/" exact component={LandingPage} />{" "}
-      <Route path="/vlogger/rec" exact component={VloggersPage} />{" "}
-      {/* NOTE: CHANGE VLOGGER TO USERS ACTUAL USERNAME */}{" "}
-      <Route path="/videopage" exact component={ViewVideoPage} />{" "}
-      {/* NOTE: CHANGE VIDEOPAGE TO USERS NAME VIDEO */}{" "}
-      <Route path="/vlogger" exact component={VideoCard} />{" "}
-      <Route path="/signup" exact component={SignUp}>
-        {" "}
-      </Route>{" "}
-      <Route path="/login" exact component={Login}>
-        {" "}
-      </Route>{" "}
-      <Route path="/signup" exact component={SignUp} />{" "}
-      <Route path="/search/:query" component={Search} />{" "}
+      <Route path="/" component={Navbar} />
+      <Route path="/" exact component={LandingPage} />
+      <Route path="/vlogger/rec" exact component={VloggersPage} />
+      {/* NOTE: CHANGE VLOGGER TO USERS ACTUAL USERNAME */}
+      <Route path="/videopage" exact component={ViewVideoPage} />
+      {/* NOTE: CHANGE VIDEOPAGE TO USERS NAME VIDEO */}
+      <Route path="/vlogger" exact component={VideoCard} />
+      <Route path="/signup" exact component={SignUp} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/search/:query" component={Search} />
     </BrowserRouter>
   );
 }

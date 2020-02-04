39199e02a15b23cbb4d26eec4e58d6b1c59659b1
Richard Green <richardgreen@pursuit.org>
2020-02-04 00:13:41 -0500

    call with backend api for products

--- src/pages/VloggersPage/VloggersPage.jsx
+++ src/pages/VloggersPage/VloggersPage.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Products from "../../components/Product-recommendations/products";
 import "./VloggersPage.css";
 import UserProfile from "../../components/UserProfile/UserProfile";
 import ToggleProductVideo from "../../components/ToggleProductVideo/ToggleProductVideo";
 import RecommendedVideo from "../../components/RecommendVideo/RecommendedVideo";
+import axios from "axios";
+
 const VloggersPage = () => {
   const [toggleVideoAndProducts, setToggleVideoAndProducts] = useState(false);
+
+  // IF I have a user pass uid into products and video
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3004/user/products`, {
+        data: {
+          id: 1
+        }
+      })
+      .then(d => {
+        console.log("datatatataat", d);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }, []);
 
   return (
     <div

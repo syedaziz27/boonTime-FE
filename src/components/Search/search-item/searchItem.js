38e4bc67e8efeb4500ec78db4eec2f2507c3836d
Richard Green <richardgreen@pursuit.org>
2020-02-10 19:57:13 -0500

    products used are in position similar to the mock up. keep up the good work Rich

--- src/components/Search/search-item/searchItem.js
+++ src/components/Search/search-item/searchItem.js
@@ -6,28 +6,29 @@ export default (props) => {
         {props.results.map((e, i) => {
             return (<React.Fragment key={i}>
                 {/* <div className="item-3"></div> */}
-                <div className="item-3">
+                <div className="item-3 bottom_line">
                     <img src={cream} width="100%"></img>
                 </div>
                 <div className="item-7 bottom_line">
                     <h1 className="item-12">{e.title}</h1>
                     <div className="row-search">
-                        <span>1k Views</span> * <span>3 hours ago</span>
+                        <span className='item-3'>1k Views * 3 hours ago</span>
 
                     </div>
                     <div className="row-search used-products">
-                        <span>Products used:</span>
+                        <span className='item-3 '>Skin Type: Sensitive</span>
+                        <span className='item-3 used-products'>Products used:</span>
                     </div>
 
 
 
 
 
-                    <div className="row-search bottom_line">
-                        <img className="item-2" src={cream} width="50%"></img>
-                        <img className="item-2" src={cream} width="50%" width="50%"></img>
-                        <img className="item-2" src={cream} width="50%" width="50%"></img>
-                        <p className="more-items item-2">Show more</p>
+                    <div className="row-search">
+                        <img className="item-1" src={cream} width="100%" alt='#' />
+                        <img className="item-1" src={cream} width="100%" alt='#' />
+                        <img className="item-1" src={cream} width="100%" alt='#' />
+                        <p className="more-items item-2 pointer">More...</p>
                         <div className="row-search item-2">
                             <span className="user-metadata-name item-6">Kylie</span>
                             <span className=" user-metadata-sub-count item-6">Subscriber count</span>
